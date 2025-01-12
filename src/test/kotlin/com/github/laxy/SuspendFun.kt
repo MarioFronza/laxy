2b4ba4e6aaa40f6427ba5f9ce1444578f3a44c54
MarioFronza <mariofronza1@gmail.com>
2025-01-12 14:09:12 -0300

    fix detekt test workflow

--- src/test/kotlin/com/github/laxy/SuspendFun.kt
+++ src/test/kotlin/com/github/laxy/SuspendFun.kt
@@ -3,6 +3,7 @@ package com.github.laxy
 import io.kotest.common.runBlocking
 import io.kotest.core.spec.style.FreeSpec
 
+@Suppress("UnnecessaryAbstractClass")
 abstract class SuspendFun(body: suspend FreeSpec.() -> Unit) : FreeSpec() {
     init {
         runBlocking { body() }
