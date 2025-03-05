58b1d9a80b8f8ff84a1c71d75be27c2cff3a07fd
MarioFronza <mariofronza1@gmail.com>
2025-03-05 10:15:49 -0300

    new css

--- src/main/kotlin/com/github/laxy/web/WebRoutes.kt
+++ src/main/kotlin/com/github/laxy/web/WebRoutes.kt
@@ -3,25 +3,72 @@ package com.github.laxy.web
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.application.install
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.respond
+import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import io.ktor.server.thymeleaf.Thymeleaf
 import io.ktor.server.thymeleaf.ThymeleafContent
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+import org.thymeleaf.templateresolver.FileTemplateResolver
 
-fun Application.configureTemplating(){
+fun Application.configureTemplating() {
     install(Thymeleaf) {
-        setTemplateResolver(ClassLoaderTemplateResolver().apply {
-            prefix = "templates/"
-            suffix = ".html"
-            characterEncoding = "UTF-8"
-        })
+        setTemplateResolver(
+            (if (developmentMode) {
+                FileTemplateResolver().apply {
+                    cacheManager = null
+                    prefix = "src/main/resources/templates/"
+                }
+            } else {
+                ClassLoaderTemplateResolver().apply {
+                    prefix = "templates/"
+                }
+            }).apply {
+                suffix = ".html"
+                characterEncoding = "utf-8"
+                addDialect(LayoutDialect())
+            })
     }
-
     routing {
+        staticResources("/static", "static")
+
         get("/") {
-            call.respond(ThymeleafContent("index", mapOf("title" to "Welcome to Laxy!")))
+            call.respond(ThymeleafContent("index", emptyMap()))
+        }
+
+        get("/signin") {
+            call.respond(ThymeleafContent("signin", emptyMap()))
+        }
+
+        post("/signin") {
+            val params = call.receiveParameters()
+            val email = params["email"]
+            val password = params["password"]
+
+            if (email == "user@example.com" && password == "password") {
+                call.respondRedirect("/")
+            } else {
+                call.respond(ThymeleafContent("signin", mapOf("error" to "Invalid credentials")))
+            }
+        }
+
+        get("/signup") {
+            call.respond(ThymeleafContent("signup", emptyMap()))
+        }
+
+        post("/signup") {
+            val params = call.receiveParameters()
+            val username = params["username"]
+            val email = params["email"]
+            val password = params["password"]
+
+            // TODO: Save user in database
+            call.respondRedirect("/signin")
         }
     }
 }
